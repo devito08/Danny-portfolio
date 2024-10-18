3b643532e1d488c0bce159617c50832cee4348d5
danny <dannydevito.cse@gmail.com>
2024-10-18 13:53:24 +0530

    personal info change

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
+    email: 'dannydevito.cse@gmail.com',
+    phone: '+919841667119',
     address: 'New perungalathur,Chennai - 600 063 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    github: 'https://github.com/devito08/',
+    facebook: 'https://www.instagram.com/danny_fc_player/',
+    linkedIn: 'https://www.linkedin.com/in/Danny-Devito/',
+    twitter: 'https://x.com/Danny_Devito_08',
+    // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "devit08"
 }
