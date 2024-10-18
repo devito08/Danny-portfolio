69aea028c049cf2d12a1fb6483184e0bbeff5cc6
danny <dannydevito.cse@gmail.com>
2024-10-18 13:25:24 +0530

    updated address

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,13 +5,13 @@ export const personalData = {
   description: "My name is DANNY DEVITO. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'dannydevito.cse@gmail.com',
   phone: '+919841667119',
-  address: 'Middle Badda, Dhaka, chennai - 1212 ',
+  address: 'New Perungalathur, Chennai - 600 063',
   github: 'https://github.com/devito08/',
   facebook: 'https://www.instagram.com/danny_fc_player/',
   linkedIn: 'https://www.linkedin.com/in/Danny-Devito/',
   twitter: 'https://x.com/Danny_Devito_08',
   stackOverflow: 'https://stackoverflwow.com/users/16840768/abu-said',
   leetcode: "https://leetcode.com/u/Danny_Devito/",
-  devUsername: "said7388",
+  devUsername: "danny_08",
   resume: "https://drive.google.com/file/d/10w6djmCevqTITEmbq0NmrkR1gscTdepm/view?usp=sharing"
 }
